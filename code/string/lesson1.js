a542ce1556554043671b2d6d207bea8955a0a25c
xypecho <xypecho@163.com>
2020-02-19 14:27:38 +0800

    备份一下，开始配置jest的testRegex

---
+++ code/string/lesson1.js
@@ -0,0 +1,5 @@
+// 翻转字符串
+
+export default (str) => {
+    return '123';
+}
---
+++ test/string/lesson1.js
@@ -0,0 +1,5 @@
+const reseve = require('../../code/string/lesson1');
+
+test('翻转字符串', () => {
+    expect(reseve('dd')).toBe('123');
+})
